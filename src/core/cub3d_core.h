b94986352326980ea75137d10c57893dae1894fc
Luccas Rocigno <luccasrocigno@hotmail.com>
2021-05-14 21:22:11 -0300

    test_room

---
+++ src/core/cub3d_core.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d_core.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/14 19:17:39 by lrocigno          #+#    #+#             */
+/*   Updated: 2021/05/14 19:44:09 by lrocigno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Header containing all the core functions of cub3D.
+*/
+
+#ifndef CUB3D_CORE_H
+# define CUB3D_CORE_H
+
+# include <libft.h>
+# include <math.h>
+# include <stdio.h>
+
+#endif
